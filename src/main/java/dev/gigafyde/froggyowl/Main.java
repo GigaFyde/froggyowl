80e7c2b615c4904dc58748abd663027f14805cdc
Laurant Marijnissen <laurant@gigafyde.dev>
2021-02-09 22:48:27 +0100

    Implement Sentry logging

--- src/main/java/dev/gigafyde/froggyowl/Main.java
+++ src/main/java/dev/gigafyde/froggyowl/Main.java
@@ -4,6 +4,7 @@ import dev.gigafyde.froggyowl.commands.CommandList;
 import dev.gigafyde.froggyowl.core.Client;
 import dev.gigafyde.froggyowl.database.Database;
 import dev.gigafyde.froggyowl.listeners.ServerLogListener;
+import io.sentry.Sentry;
 import java.util.Collections;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,20 +23,27 @@ public class Main {
     public static Database database;
     public static OkHttpClient httpClient = new OkHttpClient.Builder().protocols(Collections.singletonList(Protocol.HTTP_1_1)).build();
 
-    public static void main(String[] args) throws Exception {
-        database = new Database();
-        Client client = new Client();
-        jda = JDABuilder.createDefault(System.getenv("TOKEN"))
-                .addEventListeners(client, new ServerLogListener())
-                .enableIntents(GatewayIntent.GUILD_MEMBERS)
-                .setMemberCachePolicy(MemberCachePolicy.ALL)
-                .build();
-        new CommandList(client);
-        jda.awaitReady();
-        ServerLogListener.serverlog = jda.getTextChannelById(System.getenv("SERVERLOG_CHANNEL_ID"));
-        TimerTask timerTask = new StatusTimer();
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(timerTask, 600, 900000);
-        log.info("Started statustimer");
+    public static void main(String[] args) {
+        try {
+            Sentry.init(options -> {
+                options.setDsn(System.getenv("SENTRY_DSN"));
+            });
+            database = new Database();
+            Client client = new Client();
+            jda = JDABuilder.createDefault(System.getenv("TOKEN"))
+                    .addEventListeners(client, new ServerLogListener())
+                    .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                    .setMemberCachePolicy(MemberCachePolicy.ALL)
+                    .build();
+            new CommandList(client);
+            jda.awaitReady();
+            ServerLogListener.serverlog = jda.getTextChannelById(System.getenv("SERVERLOG_CHANNEL_ID"));
+            TimerTask timerTask = new StatusTimer();
+            Timer timer = new Timer(true);
+            timer.scheduleAtFixedRate(timerTask, 600, 900000);
+            log.info("Started statustimer");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
     }
 }
